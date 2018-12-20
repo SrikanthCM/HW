07092ecb6ff67046e5c7510a40daa4cddab782b5
shyam <shyamarjan143@gmail.com>
2018-12-21 00:35:29 +0530

    second push

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const http = require('http');
 
+var express = require('express');
+var app = express();
+
 const port=process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
@@ -11,6 +14,13 @@ res.setHeader('Content-Type', 'text/html');
 res.end('<h1>Hello World</h1>');
 
 });
+
+app.get('/sample',function(req,res)
+{
+console.log("Hello ITG !!!");
+res.send("Hi This is Manoj")
+});
+
 
 server.listen(port,() => {
 

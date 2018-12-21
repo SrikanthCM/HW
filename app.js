3236eeb8c19f5720550212f832a48880ab6ca28d
shyam <shyamarjan143@gmail.com>
2018-12-21 14:20:07 +0530

    Third Push

--- app.js
+++ app.js
@@ -2,17 +2,37 @@ const http = require('http');
 
 const port= process.env.port || process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
+var express = require('express');
+var app = express();
 
+
+app.get('/',function(req,res)
+{
 res.statusCode = 200;
 
 res.setHeader('Content-Type', 'text/html');
 
-res.end(' <font color="red">Hello world</font> ');
-
+res.end(' <h1><font color="red">Hello world</font></h1> ');
 });
 
-server.listen(port,() => {
+app.get('/itg',function(req,res)
+{
+console.log("Hello ITG Folks!!!");
+res.send("Hello ITG Folks!!!")
+});
+
+app.get('/sample',function(req,res)
+{
+console.log("Hello ITG !!!");
+res.send("Hi This is Manoj")
+});
+app.get('/itg',function(req,res)
+{
+console.log("Hello ITG Folks!!!");
+res.send("Hello ITG Folks!!!")
+});
+
+app.listen(port,() => {
 
 console.log(`Server running at port `+port);
 

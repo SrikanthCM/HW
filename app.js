b6695b2d9586175c18f027d41b73fc6cc5bd3eb3
jeevakiran <45842058+jeevakiran@users.noreply.github.com>
2018-12-21 17:00:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ res.statusCode = 200;
 
 res.setHeader('Content-Type', 'text/html');
 
-res.end('<html><body><h2>HTML Forms</h2><form action="/action_page.php">First name:<br><input type="text" name="firstname" value="Mickey"><br>Last name:<br><input type="text" name="lastname" value="Mouse"><br><br><input type="submit" value="Submit"></form> <p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p></body></html>');
+res.end('<html><body><h2>HTML Forms</h2><form action="www.google.co.in">First name:<br><input type="text" name="firstname" value="Mickey"><br>Last name:<br><input type="text" name="lastname" value="Mouse"><br><br><input type="submit" value="Submit"></form> <p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p></body></html>');
 
 });
 
